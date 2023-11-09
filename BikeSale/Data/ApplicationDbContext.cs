1fbf9b20ad52f3be42493b76a72a8379aaa7e659
antepavkovic <ante.pavkovic357@gmail.com>
2023-11-09 14:19:46 +0100

    Add project files.

---
+++ BikeSale/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BikeSale.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeSale.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+     
+    }
+}
